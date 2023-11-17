15990d90fc565b440aeefaa1ede83bb3bfc7ed60
Falk <falk.stock1@gmail.com>
2023-11-17 20:41:53 +0100

    App now askes for camera permission

--- PeekAReadApp/app/src/main/java/com/example/peekareadapp/MainActivity.kt
+++ PeekAReadApp/app/src/main/java/com/example/peekareadapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.peekareadapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -10,8 +14,11 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.peekareadapp.databinding.ActivityMainBinding
-
+import com.google.android.material.snackbar.Snackbar
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,20 +32,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        // Permissions stuff
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+        }
 
+        // Navigation stuff
+        setSupportActionBar(binding.toolbar)
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         // Set up destination change listener
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            // Check if the new destination is CameraScreen
+            // Check if the new destination is CameraScreen.
             if (destination.id == R.id.CameraScreen) {
-                // Hide the back button when in CameraScreen
+                // Hide the back button when in CameraScreen.
                 supportActionBar?.setDisplayHomeAsUpEnabled(false)
             } else {
-                // Show the back button for other destinations
+                // Show the back button for other destinations.
                 supportActionBar?.setDisplayHomeAsUpEnabled(true)
             }
         }
@@ -53,7 +67,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_settings -> {
-                // Replace with the actual navigation action for the desired screen
+                // Replace with the actual navigation action for the desired screen.
                 findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_toPreferencesScreen)
                 true
             }
@@ -69,11 +83,49 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-        // Check if the current destination is CameraScreen
+        // Check if the current destination is CameraScreen.
         if (navController.currentDestination?.id == R.id.CameraScreen) {
-            // Nullify the back action when in CameraScreen
+            // Nullify the back action when in CameraScreen.
         } else {
             super.onBackPressed()
         }
     }
+
+    // Called automatically when checking permissions.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can now use the camera.
+            } else {
+                // Permission denied, show a message to the user.
+                showPermissionDeniedMessage()
+            }
+    }
+
+    // Shows a message, that leads to the app settings, if permissions are not granted yet.
+    private fun showPermissionDeniedMessage() {
+        Snackbar.make(
+            binding.root,
+            "FEHLER: Kamerazugriff ist nötig zum Verwenden der App.",
+            Snackbar.LENGTH_INDEFINITE
+        ).setAction("Einstellungen") {
+            // Open app settings to allow the user to grant permission.
+            openAppSettings()
+        }.show()
+    }
+
+    // Opens the app settings to allow the user to grant permissions.
+    private fun openAppSettings() {
+        val intent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.parse("package:" + this.packageName)
+        )
+        startActivity(intent)
+    }
+
 }
+
