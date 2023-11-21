dbc35c7a7bc7ed2c5bd9952c4173a55c51ddc373
lrazsuh <104195048+lrazsuh@users.noreply.github.com>
2023-11-21 17:51:33 +0100

    Anpassung des Theme Namens
    Damit er mit dem Namen in der MainActivity übereinstimmt

--- PeekAReadApp/app/src/main/java/com/example/peekareadapp/Theme.kt
+++ PeekAReadApp/app/src/main/java/com/example/peekareadapp/Theme.kt
@@ -73,7 +73,7 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun AppTheme(
+fun PeekAReadTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
